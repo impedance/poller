ae47b406b42236ab980fc0c9503194dd1bfd9cc4
qvantt <2275399@gmail.com>
2022-12-26 19:33:37 +0300

    new files

---
+++ poller_phx/lib/poller_phx_web/controllers/disctrict_controller.ex
@@ -0,0 +1,14 @@
+defmodule PollerPhxWeb.DistrictController do
+  use PollerPhxWeb, :controller
+  
+  alias PollerDal.Districts
+
+  def index(conn, _params) do
+    districts = Districts.list_districts()
+    render(conn, "index.html", districts: districts)
+  end
+  
+  def new(conn, _params) do
+    render(conn, "new.html")
+  end
+end
