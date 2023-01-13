cc7ba1ef45e7ca5d69c7d58e48787203e4fdb4bd
qvantt <2275399@gmail.com>
2023-01-13 17:23:48 +0300

    add

--- poller_phx/lib/poller_phx_web/controllers/district_controller.ex
+++ poller_phx/lib/poller_phx_web/controllers/district_controller.ex
@@ -1,6 +1,6 @@
 defmodule PollerPhxWeb.DistrictController do
   use PollerPhxWeb, :controller
-  
+
   alias PollerDal.Districts
   alias PollerDal.Districts.District
 
@@ -8,27 +8,27 @@ defmodule PollerPhxWeb.DistrictController do
     districts = Districts.list_districts()
     render(conn, "index.html", districts: districts)
   end
-  
+
   def new(conn, _params) do
     changeset = Districts.change_district(%District{})
     render(conn, "new.html", changeset: changeset)
   end
-  
+
   def create(conn, %{"district" => district_params}) do
     case Districts.create_district(district_params) do
-      {:ok, _district} -> 
+      {:ok, _district} ->
         conn
         |> put_flash(:info, "District created")
         |> redirect(to: Routes.district_path(conn, :index))
-        
+
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
   end
-  
+
   def update(conn, %{"id" => id, "district" => district_params}) do
     district = Districts.get_district!(id)
-    
+
     case Districts.update_district(district, district_params) do
       {:ok, _district} ->
         conn
@@ -39,9 +39,19 @@ defmodule PollerPhxWeb.DistrictController do
         render(conn, "edit.html", district: district, changeset: changeset)
     end
   end
-  
+
   def edit(conn, %{"id" => id}) do
     district = Districts.get_district!(id)
     changeset = Districts.change_district(district)
     render(conn, "edit.html", district: district, changeset: changeset)
+  end
+
+  def delete(conn, %{"id" => id}) do
+    district = Districts.get_district!(id)
+    {:ok, _district} = Districts.delete_district(district)
+
+    conn
+    |> put_flash(:info, "District deleted")
+    |> redirect(to: Routes.district_path(conn, :index))
+  end
 end
